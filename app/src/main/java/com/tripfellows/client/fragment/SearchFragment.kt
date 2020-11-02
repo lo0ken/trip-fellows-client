129e8adfa3563552d57de4e0f8dff58d14055198
Danni <danilandroid2000@gmail.com>
2020-11-03 00:58:05 +0300

    Add SearchFragment

--- app/src/main/java/com/tripfellows/client/fragment/SearchFragment.kt
+++ app/src/main/java/com/tripfellows/client/fragment/SearchFragment.kt
@@ -5,11 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tripfellows.client.MyAdapter
 import com.tripfellows.client.R
+import java.util.*
 
 class SearchFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    var recyclerView: RecyclerView? = null
+    var adapter: MyAdapter? = null
+    var items: ArrayList<String>? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         super.onCreate(savedInstanceState)
         return inflater.inflate(R.layout.search_fragment, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        items = ArrayList()
+        items!!.add("Moscow")
+        items!!.add("Himki")
+        items!!.add("Odintsovo")
+        items!!.add("Krasnogorsk")
+        items!!.add("Malahovka")
+        items!!.add("Frazino")
+        items!!.add("Korolev")
+        items!!.add("Zelenograd")
+        items!!.add("Dolgoprudny")
+
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView?.layoutManager = (LinearLayoutManager(activity!!.applicationContext))
+        adapter = MyAdapter(activity!!.applicationContext, items!!)
+        recyclerView?.adapter = adapter
     }
 }
