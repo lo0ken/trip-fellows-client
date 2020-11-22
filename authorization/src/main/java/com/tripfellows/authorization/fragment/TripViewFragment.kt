2c3dedd28835b45b75a8cc982d19c50c5591729b
Danil-dn <58553771+Danil-dn@users.noreply.github.com>
2020-11-23 01:05:55 +0300

    fix bugs and improved design
    * fix bugs and improved design
    
    * fix bugs and improved design 2

--- authorization/src/main/java/com/tripfellows/authorization/fragment/TripViewFragment.kt
+++ authorization/src/main/java/com/tripfellows/authorization/fragment/TripViewFragment.kt
@@ -1,7 +1,7 @@
 package com.tripfellows.authorization.fragment
 
-import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,17 +24,18 @@ class TripViewFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.trip_view_layout, container, false)
+        return inflater.inflate(R.layout.trip_view_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val button: Button = view.findViewById(R.id.join_btn)
         button.setOnClickListener {
-            context?.toast("approve")
+            val text = "you joined the trip!"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(context, text, duration)
+            toast.setGravity(Gravity.CENTER, 0, 0)
+            toast.show()
         }
     }
-
-    private fun Context.toast(message: CharSequence) =
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
