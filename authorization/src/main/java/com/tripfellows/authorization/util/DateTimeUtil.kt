a42673c039bef517f2db5982e106d0e1291aa6d4
Daniel <chernov.daniel0@gmail.com>
2020-12-06 00:56:15 +0300

    Bug-12: Правка форматирования и парсинга даты и времени при создании и просмотре поездки

--- authorization/src/main/java/com/tripfellows/authorization/util/DateTimeUtil.kt
+++ authorization/src/main/java/com/tripfellows/authorization/util/DateTimeUtil.kt
@@ -5,23 +5,24 @@ import java.util.*
 
 object DateTimeUtil {
     private val serverDateFormatter = SimpleDateFormat("yyyy-MM-dd'T'", Locale.ROOT)
+    private val serverDateFormatterWithTime = SimpleDateFormat("yyyy-MM-dd'T'hh:mm", Locale.ROOT)
     private val dateAndTimeFormatter = SimpleDateFormat("yyyy-MM-dd hh:mm", Locale.ROOT)
     private val timeFormatter = SimpleDateFormat("hh:mm", Locale.ROOT)
 
 
     fun formatWithDateAndTime(date: String): String {
-        return dateAndTimeFormatter.format(parseServerDate(date))
+        return dateAndTimeFormatter.format(parseServerDateWithTime(date))
     }
 
     fun formatWithTime(date: String): String {
-        return timeFormatter.format(parseServerDate(date))
+        return timeFormatter.format(parseServerDateWithTime(date))
     }
 
     fun makeServerCurrentDayWithTime(date: String): String {
         return serverDateFormatter.format(Date()) + date
     }
 
-    private fun parseServerDate(date: String): Date {
-        return serverDateFormatter.parse(date)!!
+    private fun parseServerDateWithTime(date: String): Date {
+        return serverDateFormatterWithTime.parse(date)!!
     }
 }
